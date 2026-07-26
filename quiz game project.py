9c923075a1817f5b954dad607a869a1d812da223
Paramjit Mondal <paramjitmondal32@gmail.com>
2026-07-27 01:14:50 +0530

    Add files via upload

---
+++ quiz game project.py
@@ -0,0 +1,31 @@
+question_1=str(input("Which is the largest ocean in the world?  atlantic ocean,  indian ocean,  pacific ocean,  arctic ocean:"))
+
+if question_1.lower().strip() == "pacific ocean":
+    print("YOUR ANSWER IS CORRECT")
+else:
+    print("YOUR ANSWER IS  NOT CORRECT....TRY AGAIN")
+
+
+question_2=str(input("What is known as the Brain of a computer? ram,hardisk,cpu,monitor:"))
+
+if question_2.lower().strip() == "cpu":
+    print("YOUR ANSWER IS CORRECT")
+else:
+    print("YOUR ANSWER IS  NOT CORRECT....TRY AGAIN")
+
+
+question_3=str(input("how many trophy rcb won(men team,2026)? 1,2,3,5:"))
+
+if question_3.lower().strip() == "2":
+    print("YOUR ANSWER IS CORRECT")
+else:
+    print("YOUR ANSWER IS  NOT CORRECT....TRY AGAIN")
+
+question_4=str(input("How many days are there in a week? 7days,6days,4days,8days"))
+if question_4.lower().strip()=="7 days":
+     print("YOUR ANSWER IS CORRECT")
+else:
+        print("YOUR ANSWER IS  NOT CORRECT....TRY AGAIN")
+
+        
+   
